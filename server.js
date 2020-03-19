ad1f7ca06b9d77f2ff4b5c2dc2fb4554b60e145f
unknown <12siricoa94@gmail.com>
2020-03-19 00:31:18 -0400

    server up and running

---
+++ server.js
@@ -0,0 +1,14 @@
+let express = require("express");
+
+let PORT = process.env.PORT || 8080;
+
+let app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
