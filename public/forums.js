663757040e8cac802150168747f39f38ba6d3232
unknown <12siricoa94@gmail.com>
2020-03-19 00:49:17 -0400

    able to get data from mysql database

---
+++ public/forums.js
@@ -0,0 +1,31 @@
+$.ajax("/data", {
+    type: "GET"
+}).then(function(data) {
+    console.log(data);
+    console.log(data.forum);
+    let forumList= data.forum;
+    let length = forumList.length;
+
+    for(let i = 0; i < length; i++){
+        let newList = "<li>" + "<h1>"+ forumList[i].username + "</h1>" + "<p>"+ forumList[i].post + "</p>"+"</li>"
+        $("#forum").append(newList);
+    }
+});
+$('#post').on('click', function(event){
+    console.log('thisworks!');
+    event.preventDefault();
+
+    let newpost = {
+        username: $("#postTitle").val(),
+        post: $("#postBody").val()
+    }
+    console.log("this is the " + JSON.stringify(newpost));
+    $.ajax("/api/forum", {
+        type: "POST",
+        data: newpost
+      }).then(function() {
+        console.log("created new post");
+        // Reload the page to get the updated list
+        location.reload();
+      });
+});
--- router/router.js
+++ router/router.js
@@ -4,11 +4,18 @@ let router = express.Router();
 
 let path = require("path");
 
+let forum = require("../models/forum");
+
 router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 router.get("/forum", function(req, res){
     res.sendFile(path.join(__dirname, "../public/forum.html"));
+});
+router.get("/data", function(req, res) {
+    forum.all(function(data) {
+      res.json({ forum: data });
+    });
 });
 
 module.exports = router;
