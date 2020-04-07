28de446b66c2a0d58d2f37fba42fdcdd40097939
unknown <12siricoa94@gmail.com>
2020-04-07 16:14:44 -0400

    changed connection.js to try to solve heroku issue

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+let mysql = require("mysql");
+let dbConnect = require("./dbconnection");
+
+if (process.env.JAWSDB_URL) {
+   connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = dbConnect;
+}
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+});
+
+connection.config.typeCast = function(field, next) {
+  if (field.type == "TINY" && field.length == 1) {
+    return field.string() == "1"; // 1 = true, 0 = false
+  }
+  return next();
+};
+
+module.exports = connection;
